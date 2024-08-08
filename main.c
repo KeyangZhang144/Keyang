619a7edb7d6adb429dbbac5326c89130c2000a0a
KeyangZhang144 <zhangkeyang144@gmail.com>
2024-08-08 12:57:40 +0800

    add main.c

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
-#include <stdio.h>
+/**
+ * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 #include "pico/stdlib.h"
+#include "pico/cyw43_arch.h"
+#include "hcsr04.h"
 
 int main() {
+    timer_hw->dbgpause = 0;
     stdio_init_all();
-    printf("Hello, world!\n");
-    return 0;
+    int distance;
+    while(true){
+        hcsr04_init();
+        distance = hcsr04_get_distance();
+        printf("distance : %d\n", distance);
+        //printf("distance : %.2f\n", distance);
+        busy_wait_ms(500);
+    }
+/*    while (true) {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(250);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(250);
+    }
+    */
 }
